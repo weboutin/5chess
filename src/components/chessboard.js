aee1c602b33a70f9b084efcaa096db35a23c8890
itgo <634872896@qq.com>
2019-10-10 16:14:21 +0800

    feat: handle click event

--- src/components/chessboard.js
+++ src/components/chessboard.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 export default class Chessboard extends React.Component {
   componentDidMount() {
-    let c = document.getElementById("chessboard");
-    let ctx = c.getContext("2d");
+    this.c = document.getElementById("chessboard");
+    this.c.addEventListener("click", (e) => this.clickBoard(e), false)
+    let ctx = this.c.getContext("2d");
+    this.ctx = ctx
     for (let i = 1; i < 16; i++) {
       ctx.moveTo(i * 30, 30);
       ctx.lineTo(i * 30, 450);
@@ -48,6 +50,32 @@ export default class Chessboard extends React.Component {
     ctx.closePath();
     ctx.fill();
     ctx.stroke();;
+  }
+
+  draw_chess(x_line, y_line, color) {
+    let ctx = this.ctx
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x_line * 30, y_line * 30, 10, 0, 2 * Math.PI, true);
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+  }
+
+  clickBoard(e) {
+    let x = e.pageX;
+    let y = e.pageY;
+    x -= this.c.offsetLeft;
+    y -= this.c.offsetTop;
+    let x_line = parseInt(Number(x / 30).toFixed())
+    let y_line = parseInt(Number(y / 30).toFixed())
+    if (x_line > 15 || x_line < 1 || y_line > 15 || y_line < 1) {
+      return
+    }
+    this.x_line = x_line
+    this.y_line = y_line
+    this.draw_chess(x_line, y_line, 'black')
+
   }
 
   render() {
