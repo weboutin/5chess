9e0f5c2424dac6e2544c14b024a85ba8b02c39b7
itgo <634872896@qq.com>
2019-10-14 17:50:51 +0800

    feat: 完成胜负规则

--- src/components/chessboard.js
+++ src/components/chessboard.js
@@ -4,6 +4,14 @@ import React from 'react';
 const BLACK_CHESS = 0
 const WHITE_CHESS = 1
 const EMPATY_CHESS = -1
+//水平
+const HORIZONTAL = 'horizontal'
+//垂直
+const VERTICAL = 'vertical'
+//45倾斜方向
+const DEGREE45 = 'degree45'
+//135倾斜方向
+const DEGREE135 = 'degree135'
 
 export default class Chessboard extends React.Component {
   constructor() {
@@ -25,7 +33,12 @@ export default class Chessboard extends React.Component {
   }
 
   setChessContainer(x_line, y_line, currentChessColor) {
+    //禁止下子在同一个地方
+    if (this.chessContainer[x_line - 1][y_line - 1] != -1) {
+      return false
+    }
     this.chessContainer[x_line - 1][y_line - 1] = currentChessColor
+    return true
   }
 
   initBlack(x, y) {
@@ -84,9 +97,76 @@ export default class Chessboard extends React.Component {
     }
     this.x_line = x_line
     this.y_line = y_line
+    let isSetChess = this.setChessContainer(x_line, y_line, this.currentChessColor);
+    if (!isSetChess) {
+      return
+    }
     this.draw_chess(x_line, y_line, this.currentChessColor)
+    let isWin = this.isWin(x_line, y_line, this.currentChessColor)
+    isWin && alert('win')
+    //下一个子设置为相反颜色
     this.currentChessColor = (this.currentChessColor === BLACK_CHESS ? WHITE_CHESS : BLACK_CHESS)
-    this.setChessContainer(x_line, y_line, this.currentChessColor);
+  }
+
+  //寻找各个方位，是否有连续5个子
+  isWin(x_line, y_line, currentChessColor) {
+    //将canvas坐标转化为数组坐标
+    let x = x_line - 1
+    let y = y_line - 1
+    let isWin = false
+    isWin = this.getContinusChess(x, y, currentChessColor, HORIZONTAL) >= 5
+    if (isWin) return true
+    isWin = this.getContinusChess(x, y, currentChessColor, VERTICAL) >= 5
+    if (isWin) return true
+    isWin = this.getContinusChess(x, y, currentChessColor, DEGREE45) >= 5
+    if (isWin) return true
+    isWin = this.getContinusChess(x, y, currentChessColor, DEGREE135) >= 5
+    if (isWin) return true
+    return false
+  }
+
+  addContinusChessByDirect(x, y, currentChessColor, direct) {
+    if (this.chessContainer[x][y] === -1) {
+      return 0
+    }
+    if (this.chessContainer[x][y] !== currentChessColor) {
+      return 0
+    }
+    switch (direct) {
+      case 'x+':
+        return this.addContinusChessByDirect(x + 1, y, currentChessColor, 'x+') + 1
+      case 'x-':
+        return this.addContinusChessByDirect(x - 1, y, currentChessColor, 'x-') + 1
+      case 'y+':
+        return this.addContinusChessByDirect(x, y + 1, currentChessColor, 'y+') + 1
+      case 'y-':
+        return this.addContinusChessByDirect(x, y - 1, currentChessColor, 'y-') + 1
+      case 'x+y+':
+        return this.addContinusChessByDirect(x + 1, y + 1, currentChessColor, 'x+y+') + 1
+      case 'x-y-':
+        return this.addContinusChessByDirect(x - 1, y - 1, currentChessColor, 'x-y-') + 1
+      case 'x-y+':
+        return this.addContinusChessByDirect(x - 1, y + 1, currentChessColor, 'x-y+') + 1
+      case 'x+y-':
+        return this.addContinusChessByDirect(x + 1, y - 1, currentChessColor, 'x+y-') + 1
+    }
+  }
+
+  getContinusChess(x, y, currentChessColor, direct) {
+    switch (direct) {
+      case HORIZONTAL:
+        return this.addContinusChessByDirect(x + 1, y, currentChessColor, 'x+') +
+          this.addContinusChessByDirect(x - 1, y, currentChessColor, 'x-') + 1
+      case VERTICAL:
+        return this.addContinusChessByDirect(x, y + 1, currentChessColor, 'y+') +
+          this.addContinusChessByDirect(x, y - 1, currentChessColor, 'y-') + 1
+      case DEGREE45:
+        return this.addContinusChessByDirect(x + 1, y + 1, currentChessColor, 'x+y+') +
+          this.addContinusChessByDirect(x - 1, y - 1, currentChessColor, 'x-y-') + 1
+      case DEGREE135:
+        return this.addContinusChessByDirect(x - 1, y + 1, currentChessColor, 'x-y+') +
+          this.addContinusChessByDirect(x + 1, y - 1, currentChessColor, 'x+y-') + 1
+    }
   }
 
   render() {
